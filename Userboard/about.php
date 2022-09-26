01642e17dfee3514cd6aead2dae45961d592f46a
husseinadq <husseinadq2@gmail.com>
2022-09-26 07:11:40 +0300

    create About page

---
+++ Userboard/about.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+    <title>About</title>
+</head>
+<body>
+<div class="container-lg">
+    <div class="row justify-content-center my-5">
+        <div class="col-lg-6 mt-4 ">
+            <h2 class="text-center  fst-normal">About </h2>
+            <p class="text-center fs-4 fst-normal mb-5"> use and not just for display purposes. A library provides physica</p>   
+        </div>   
+    </div>
+</div>
+</body>
